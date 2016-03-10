a1a1198dab710b8d9a70c1e167e3688aa6206f62
Simon Jespersen <s.jespersen@stud.uis.no>
2016-03-10 23:56:45 +0100

    factory

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,111 +1,120 @@
 angular.module('geekeventsApp.controllers', [])
 
-    .controller('MenuCtrl', ['$scope', '$ionicModal', '$timeout', '$state', '$ionicHistory', 'apiFactory', function($scope, $ionicModal, $timeout, $state, $ionicHistory, apiFactory) {
+    .controller('MenuCtrl', ['$scope', '$ionicModal', '$timeout', '$state', '$ionicHistory', 'apiFactory',
+        function($scope, $ionicModal, $timeout, $state, $ionicHistory, apiFactory) {
 
-        $scope.menuOptions = [
-            {
-                menuText: 'All Events',
-                state: 'allEvents'
-            },
-            {
-                menuText: 'Local Events',
-                state: 'localEvents'
-            },
-            {
-                menuText: 'Gaming Events',
-                state: 'gameEvents'
-            },
-            {
-                menuText: 'Cosplay Events',
-                state: 'cosplayEvents'
-            },
-            {
-                menuText: 'Board Game Events',
-                state: 'boardEvents'
-            },
-            {
-                menuText: 'Other Events',
-                state: 'otherEvents'
+            $scope.menuOptions = [
+                {
+                    menuText: 'All Events',
+                    state: 'allEvents'
+                },
+                {
+                    menuText: 'Local Events',
+                    state: 'localEvents'
+                },
+                {
+                    menuText: 'Gaming Events',
+                    state: 'gameEvents'
+                },
+                {
+                    menuText: 'Cosplay Events',
+                    state: 'cosplayEvents'
+                },
+                {
+                    menuText: 'Board Game Events',
+                    state: 'boardEvents'
+                },
+                {
+                    menuText: 'Other Events',
+                    state: 'otherEvents'
+                }
+            ]
+            $scope.currentState = "allEvents";
+            $scope.eventList = {};
+
+            $scope.getEvents = function(state) {
+                switch (state) {
+                    case 'allEvents':
+                        apiFactory.getAllEvents().success(function(result) {
+                            $scope.eventList = result;
+                            console.log(result);
+                        });
+                        break;
+                    case 'localEvents':
+                        $scope.eventList = {};
+                        break;
+                    case 'gameEvents':
+                        getEventsByType('lan');
+                        break;
+                    case 'cosplayEvents':
+                        getEventsByType('cosplay');
+                        break;
+                    case 'boardEvents':
+                        getEventsByType('board');
+                        break;
+                    case 'otherEvents':
+                        getEventsByType('other');
+                        break;
+                }
             }
-        ]
-        $scope.currentState = "allEvents";
 
-        $scope.getEvents = function(state) {
-            switch (state) {
-                case 'allEvents':
-                    console.log("all");
-                    apiFactory.getAllEvents().success(function (result) {
-                        console.log(result);
+            $scope.goToPage = function(state) {
+                if (state != $scope.currentState) {
+                    $ionicHistory.nextViewOptions({
+                        disableBack: true
                     });
-                    break;
-                case 'localEvents':
-                    console.log("local");
-                    break;
-                case 'gameEvents':
-                    console.log('only games');
-                    break;
-                case 'cosplayEvents':
-                    console.log('cosplayevents');
-                    break;
-                case 'boardEvents':
-                    console.log('board game');
-                    break;
-                case 'otherEvents':
-                    console.log('otherEvents');
-                    break;
+                    $state.go('app.' + state);
+                    $scope.currentState = state;
+                    $scope.getEvents(state);
+                }
             }
-        }
 
-        $scope.goToPage = function(state) {
-            if (state != $scope.currentState) {
-                $ionicHistory.nextViewOptions({
-                    disableBack: true
+            $scope.isThisCurrentState = function(state) {
+                return state == $scope.currentState;
+            }
+
+            function getEventsByType(type) {
+                apiFactory.getEventsByType(type).success(function(result) {
+                    $scope.eventList = result;
+                    console.log(result);
                 });
-                $state.go('app.' + state);
-                $scope.currentState = state;
-                $scope.getEvents(state);
             }
-        }
 
-        $scope.isThisCurrentState = function(state) {
-            return state == $scope.currentState;
-        }
+            // With the new view caching in Ionic, Controllers are only called
+            // when they are recreated or on app start, instead of every page change.
+            // To listen for when this page is active (for example, to refresh data),
+            // listen for the $ionicView.enter event:
+            //$scope.$on('$ionicView.enter', function(e) {
+            //});
 
-        // With the new view caching in Ionic, Controllers are only called
-        // when they are recreated or on app start, instead of every page change.
-        // To listen for when this page is active (for example, to refresh data),
-        // listen for the $ionicView.enter event:
-        //$scope.$on('$ionicView.enter', function(e) {
-        //});
+            // Form data for the login modal
+            $scope.loginData = {};
 
-        // Form data for the login modal
-        $scope.loginData = {};
+            // Create the login modal that we will use later
+            $ionicModal.fromTemplateUrl('templates/login.html', {
+                scope: $scope
+            }).then(function(modal) {
+                $scope.modal = modal;
+            });
 
-        // Create the login modal that we will use later
-        $ionicModal.fromTemplateUrl('templates/login.html', {
-            scope: $scope
-        }).then(function(modal) {
-            $scope.modal = modal;
-        });
+            // Triggered in the login modal to close it
+            $scope.closeLogin = function() {
+                $scope.modal.hide();
+            };
 
-        // Triggered in the login modal to close it
-        $scope.closeLogin = function() {
-            $scope.modal.hide();
-        };
+            // Open the login modal
+            $scope.login = function() {
+                $scope.modal.show();
+            };
 
-        // Open the login modal
-        $scope.login = function() {
-            $scope.modal.show();
-        };
+            // Perform the login action when the user submits the login form
+            $scope.doLogin = function() {
+                console.log('Doing login', $scope.loginData);
 
-        // Perform the login action when the user submits the login form
-        $scope.doLogin = function() {
-            console.log('Doing login', $scope.loginData);
-
-            // Simulate a login delay. Remove this and replace with your login
-            // code if using a login system
-            $timeout(function() {
-                $scope.closeLogin();
-            }, 1000);
-        };
-    }]);
+                // Simulate a login delay. Remove this and replace with your login
+                // code if using a login system
+                $timeout(function() {
+                    $scope.closeLogin();
+                }, 1000);
+            };
+        }]);
--- www/js/factories/eventFactory.js
+++ www/js/factories/eventFactory.js
@@ -6,6 +6,13 @@ angular.module('geekeventsApp')
 		apiFactory.getAllEvents = function () {
 			return $http.get(ApiEndpoint.url + "/events");
 		}
+        
+        apiFactory.getEventsByType = function (type) {
+			return $http.get(ApiEndpoint.url + "/events?type=" + type);
+		}
+        
+        //TODO: get local events
+        //add events
 
 		return apiFactory;
 	}]);
